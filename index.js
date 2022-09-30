2027b7769b3f7bbf5788f2fe20a3af9c13976429
Karlo Paz <kpaz30@alumnos.uaq.mx>
2022-09-30 12:05:39 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+/* 
+verbos HTTP
+GET
+POST
+PATCH
+PUT
+DELETE
+*/
+app.get()
+app.listen(3000,function(){
+    console.log('Server is running')
+})
+console.log('Hola mundo')
